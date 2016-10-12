6cd278d5b2ce8f7d2a00b21ee111d70dd2f73e4e
Leonidez Acosta <leonidez@poeticsystems.com>
2016-10-12 12:28:57 -0500

    create custom form submit handler

--- form.jsx
+++ form.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
 import Form from './form.html';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const parser = new DOMParser();
 
 export default class extends React.Component {
   createMarkup() {
     const html = parser.parseFromString(Form, 'text/html');
-    const htmlString = html.documentElement.querySelector('body').innerHTML
+    const body = html.documentElement.querySelector('body');
+    const form = body.querySelector('#todo-form');
+    const script = document.createElement('script');
+    script.text = 'var logResult = function () { console.log("form"); return false; }';
+    script.type = 'text/javascript'
+    document.body.appendChild(script);
+    form.setAttribute('onsubmit', 'return logResult()')
+    const htmlString = body.innerHTML
     return { __html: htmlString };
   }
   render() {
