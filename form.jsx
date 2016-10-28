05986da1998123ae4bb3bf4e0868a22558bdf17b
Leonidez Acosta <leonidez@poeticsystems.com>
2016-10-28 14:38:33 -0500

    target form in handler and remove script on unmouint

--- form.jsx
+++ form.jsx
@@ -1,19 +1,20 @@
 import Form from './form.html';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const parser = new DOMParser();
+import { getBodyElement, buildScriptWithFn } from './helpers';
 
 export default class extends React.Component {
+  logResult() {
+    const name = document.getElementById('todo-name');
+    console.log('the name', name.value);
+    return false;
+  }
   createMarkup() {
-    const html = parser.parseFromString(Form, 'text/html');
-    const body = html.documentElement.querySelector('body');
+    const body = getBodyElement(Form);
+    this.script = buildScriptWithFn(this.logResult);
+    document.body.appendChild(this.script);
     const form = body.querySelector('#todo-form');
-    const script = document.createElement('script');
-    script.text = 'var logResult = function () { console.log("form"); return false; }';
-    script.type = 'text/javascript'
-    document.body.appendChild(script);
-    form.setAttribute('onsubmit', 'return logResult()')
+    form.setAttribute('onsubmit', 'return logResult()');
     const htmlString = body.innerHTML
     return { __html: htmlString };
   }
@@ -22,4 +23,7 @@ export default class extends React.Component {
       <div dangerouslySetInnerHTML={this.createMarkup()} />
     );
   }
-}
+  componentWillUnmount() {
+    this.script.remove();
+  }
+};
